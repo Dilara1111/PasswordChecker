bf197ead0402171ab5a690253547b870b6134b50
HuseynovaDilara <huseynova.8885@gmail.com>
2023-11-25 22:03:54 +0400

    Task added

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace PasswordChecker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string testPassword = Console.ReadLine();
+            bool isValid = PasswordChecker(testPassword);
+            if (!isValid) 
+            {
+                Console.WriteLine("Invalid Password");
+            }
+            else { Console.WriteLine("You are welcome"); }
+
+               
+        }
+        static bool PasswordChecker(string password)
+        {
+            // Shifrenin 8den boyuk olmasini yoxlayir
+            if (password.Length < 8)
+            {
+                return false;
+            }
+            // Shifrenin bosh olub-olmamasini yoxlayir
+            if (string.IsNullOrEmpty(password))
+            {
+                return false;
+            }
+            // Shifrede en azi bir boyuk herf olmasini yoxlayir
+            if (!Regex.IsMatch(password, "[A - Z]+"))
+            { 
+                return false; 
+            }
+            //Shifrede en azi bir reqem olmasini yoxlayir
+            if (!Regex.IsMatch(password, @"\d+"))
+            {
+                return false;
+            }
+            // SHifrede en azi bir kicik reqem olumasini yoxlayir
+            if (!Regex.IsMatch(password, "[a-z]+"))
+            {
+                return false;
+            }
+
+            // Şifrede özel olmasını kontrol edir
+            if (!Regex.IsMatch(password, "[^a-zA-Z0-9]+"))
+            {
+                return false;
+            }
+            return true;
+
+
+
+
+        }
+    }
+}
